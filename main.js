54331acc87a7e17d5972880da8aa89512241ba9c
TheTrueHJ <101444864+TheTrueHJ@users.noreply.github.com>
2023-06-24 14:30:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(700,500);
+    canvas.position(240,200);
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+
+}
